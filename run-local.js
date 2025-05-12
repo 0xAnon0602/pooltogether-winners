ad89e46c669299c928b15856f947b73037851d29
0xAnon <utkarshshail005@protonmail.com>
2025-05-12 23:15:41 +0530

    added changes to main script

--- run-local.js
+++ run-local.js
@@ -13,7 +13,8 @@ const networks = {
     chainId: 10,
     prizePoolAddress: '0xf35fe10ffd0a9672d0095c435fd8767a7fe29b55',
     contractJsonUrl: 'https://raw.githubusercontent.com/GenerationSoftware/pt-v5-mainnet/396f04daedc5a38935460ddf47d2f10e9ac1fec6/deployments/optimism/contracts.json',
-    subgraphUrl: 'https://api.studio.thegraph.com/query/63100/pt-v5-optimism/version/latest/graphql',
+    subgraphUrl: 'https://api.studio.thegraph.com/query/63100/pt-v5-optimism/version/latest',
+    vaults: '6', 
     rpcEnvVar: 'OPTIMISM_MAINNET_RPC_URL'
   },
   'base-mainnet': {
@@ -21,6 +22,7 @@ const networks = {
     prizePoolAddress: '0x45b2010d8A4f08b53c9fa7544C51dFd9733732cb',
     contractJsonUrl: 'https://raw.githubusercontent.com/GenerationSoftware/pt-v5-mainnet/bc84c3f5e1d9703372ae7f9baf584bab42f47b27/deployments/base/contracts.json',
     subgraphUrl: 'https://subgraph.satsuma-prod.com/17063947abe2/g9-software-inc--666267/pt-v5-base/version/v0.0.1/api',
+    vaults: '0,1,2,3,4,5', 
     rpcEnvVar: 'BASE_MAINNET_RPC_URL'
   },
   'gnosis-mainnet': {
@@ -28,6 +30,7 @@ const networks = {
     prizePoolAddress: '0x0c08c2999e1a14569554eddbcda9da5e1918120f',
     contractJsonUrl: 'https://raw.githubusercontent.com/GenerationSoftware/pt-v5-mainnet/196aa20f4a0b3e651d0504ffeb0e1b9a08c7ccb6/deployments/gnosis/contracts.json',
     subgraphUrl: 'https://api.studio.thegraph.com/query/63100/pt-v5-gnosis/version/latest/graphql',
+    vaults: '0,1,2,3,4,5', 
     multicallAddress: '0xcA11bde05977b3631167028862bE2a173976CA11',
     rpcEnvVar: 'GNOSIS_MAINNET_RPC_URL'
   },
@@ -36,6 +39,7 @@ const networks = {
     prizePoolAddress: '0x99ffb0a6c0cd543861c8de84dd40e059fd867dcf',
     contractJsonUrl: 'https://raw.githubusercontent.com/GenerationSoftware/pt-v5-mainnet/8e1432b70c1f135966c1b70917675cd586dda7be/deployments/world/contracts.json',
     subgraphUrl: 'https://api.goldsky.com/api/public/project_cm3xb1e8iup5601yx9mt5caat/subgraphs/pt-v5-world/v0.0.1/gn',
+    vaults: '0,1,2,3,4,5', 
     multicallAddress: '0xcA11bde05977b3631167028862bE2a173976CA11',
     rpcEnvVar: 'WORLD_MAINNET_RPC_URL'
   }
@@ -44,7 +48,6 @@ const networks = {
 // CLI version and constants
 const CLI_VERSION = '2.0.8';
 const OUTPUT_DIRECTORY_NAME = 'winners/vaultAccounts';
-const PRIZE_TIERS_TO_COMPUTE = '0,1,2,3,4,5';
 
 // Parse command line arguments
 const args = process.argv.slice(2);
@@ -62,6 +65,8 @@ const network = networks[networkArg];
 
 // Check if RPC URL is set
 const rpcUrl = process.env[network.rpcEnvVar];
+const prizeTiers = network.vaults
+
 if (!rpcUrl) {
   console.error(`Error: ${network.rpcEnvVar} environment variable is not set.`);
   console.error(`Please set it using: export ${network.rpcEnvVar}=<your-rpc-url>`);
@@ -75,7 +80,7 @@ if (!fs.existsSync(outputDir)) {
 }
 
 // Retry logic
-const MAX_RETRIES = 50;
+const MAX_RETRIES = 1;
 let attempt = 0;
 
 while (attempt < MAX_RETRIES) {
@@ -91,7 +96,9 @@ while (attempt < MAX_RETRIES) {
     }
 
     // Set remote status URL 
+    // const remoteStatusUrl = `https://raw.githubusercontent.com/0xAnon0602/pooltogether-winners/refs/heads/main/winners/vaultAccounts`;
     const remoteStatusUrl = `https://raw.githubusercontent.com/GenerationSoftware/pt-v5-winners/refs/heads/main/winners/vaultAccounts`;
+
 
     // Run the command to compute winners
     const command = `ptv5 utils compileWinners \
@@ -110,7 +117,7 @@ while (attempt < MAX_RETRIES) {
         ...process.env,
         JSON_RPC_URL: rpcUrl,
         NODE_OPTIONS: '--max_old_space_size=32768',
-        PRIZE_TIERS_TO_COMPUTE: PRIZE_TIERS_TO_COMPUTE,
+        PRIZE_TIERS_TO_COMPUTE: prizeTiers,
         DEBUG: true
       }
     });
